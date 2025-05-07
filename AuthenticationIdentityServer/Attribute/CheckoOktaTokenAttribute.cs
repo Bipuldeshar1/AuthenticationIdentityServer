67b55e2671a893da2c58134495de0b8430701f4e
bipul.deshar <bipul.deshar@NP5OMGW03271GCK>
2025-05-07 14:10:15 +0545

    custom attribute pre setup for oktaToken Detection

---
+++ AuthenticationIdentityServer/Attribute/CheckoOktaTokenAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AuthenticationIdentityServer.Attribute
+{
+    public class CheckoOktaTokenAttribute:ActionFilterAttribute
+    {
+        public CheckoOktaTokenAttribute()
+        {
+          
+        }
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next) { }
+    }
+}
